0328e566a0b6f5359580a88cbfc5ad1c3fb28be0
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-02-29 02:46:19 +0100

    fix(error): Suspense boundary with useSearchParams

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -10,6 +10,7 @@ import { cn } from "@/lib/utils";
 import { ProductFilters } from "@/components/product-filters";
 import { ProductGrid } from "@/components/product-grid";
 import { ProductSort } from "@/components/product-sort";
+import { Suspense } from "react";
 
 interface Props {
   searchParams: {
@@ -51,36 +52,32 @@ export default async function Page({ searchParams }: Props) {
   );
 
   return (
-    <div>
-      <div>
-        <main className="mx-auto max-w-6xl px-6">
-          <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-16 dark:border-gray-800">
-            <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
-              {products.length} result{products.length === 1 ? "" : "s"}
-            </h1>
-            <ProductSort />
-          </div>
+    <Suspense>
+      <main className="mx-auto max-w-6xl px-6">
+        <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-16 dark:border-gray-800">
+          <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
+            {products.length} result{products.length === 1 ? "" : "s"}
+          </h1>
+          <ProductSort />
+        </div>
 
-          <section aria-labelledby="products-heading" className="pb-24 pt-6">
-            <h2 id="products-heading" className="sr-only">
-              Products
-            </h2>
-            <div
-              className={cn(
-                `grid grid-cols-1 gap-x-8 gap-y-10`,
-                products.length > 0
-                  ? "lg:grid-cols-4"
-                  : "lg:grid-cols-[1fr_3fr]"
-              )}
-            >
-              <div className="hidden lg:block">
-                <ProductFilters />
-              </div>
-              <ProductGrid products={products} />
+        <section aria-labelledby="products-heading" className="pb-24 pt-6">
+          <h2 id="products-heading" className="sr-only">
+            Products
+          </h2>
+          <div
+            className={cn(
+              `grid grid-cols-1 gap-x-8 gap-y-10`,
+              products.length > 0 ? "lg:grid-cols-4" : "lg:grid-cols-[1fr_3fr]"
+            )}
+          >
+            <div className="hidden lg:block">
+              <ProductFilters />
             </div>
-          </section>
-        </main>
-      </div>
-    </div>
+            <ProductGrid products={products} />
+          </div>
+        </section>
+      </main>
+    </Suspense>
   );
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { MobileNav } from "@/components/mobile-nav";
+import { Suspense } from "react";
 
 export function SiteHeader() {
   const pathname = usePathname();
@@ -26,42 +27,44 @@ export function SiteHeader() {
 
   if (pathname.startsWith("/studio")) return null;
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx-auto max-w-6xl flex h-16 items-center lg:justify-between space-x-4 px-6 sm:space-x-0">
-        <MainNav />
-        <MobileNav />
-        <div className="flex items-center space-x-1">
-          <form
-            onSubmit={onSubmit}
-            className="items-center lg:inline-flex lg:mr-4"
-          >
-            <Input
-              id="search"
-              name="search"
-              type="search"
-              autoComplete="off"
-              placeholder="Search products..."
-              className="h-9 lg:w-[300px]"
-              defaultValue={defaultSearchQuery}
-            />
-          </form>
-          <Link href="/cart">
-            <Button size="sm" variant="ghost">
-              <ShoppingCart className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
-              <span className="sr-only">Cart</span>
-            </Button>
-          </Link>
-          {process.env.NODE_ENV === "development" && <ThemeToggle />}
-          {process.env.NODE_ENV === "development" && (
-            <Link href="/studio">
+    <Suspense>
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="mx-auto max-w-6xl flex h-16 items-center lg:justify-between space-x-4 px-6 sm:space-x-0">
+          <MainNav />
+          <MobileNav />
+          <div className="flex items-center space-x-1">
+            <form
+              onSubmit={onSubmit}
+              className="items-center lg:inline-flex lg:mr-4"
+            >
+              <Input
+                id="search"
+                name="search"
+                type="search"
+                autoComplete="off"
+                placeholder="Search products..."
+                className="h-9 lg:w-[300px]"
+                defaultValue={defaultSearchQuery}
+              />
+            </form>
+            <Link href="/cart">
               <Button size="sm" variant="ghost">
-                <Edit className="h-5 w-5" />
+                <ShoppingCart className="h-5 w-5" />
+                <span className="ml-2 text-sm font-bold">0</span>
+                <span className="sr-only">Cart</span>
               </Button>
             </Link>
-          )}
+            {process.env.NODE_ENV === "development" && <ThemeToggle />}
+            {process.env.NODE_ENV === "development" && (
+              <Link href="/studio">
+                <Button size="sm" variant="ghost">
+                  <Edit className="h-5 w-5" />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </Suspense>
   );
 }
